ffe440762d7f8ee3a1612775e5454d1fd67a351f
Abdulhmeed Harrass <392024733@stu.iu.edu.sa>
2022-12-14 00:46:44 +0300

    adding hrkat function 0.1v

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import Server
-
 text = u'ذهب خالد إلى الحديقة'
 
 Tagger = Server._3RreEB("http://139.59.210.136:9005",1)
@@ -10,3 +9,8 @@ Parser = Server._3RreEB("http://139.59.210.136:9005",0)
 Parser.Draw(text)
 #print(Parser.Parse(text))
 
+hrkat = {"fatha":"ً","dmah":"ُ","ksrah":"ِ","fatha2":"ً","dmah2":"ٌ","ksrah2":"ٍ","shadah":"ّ","sokon":"ْ"}
+t = u'ولد'
+if True:
+    t = t+hrkat["dmah2"]
+print(t)
