341f47c057eab297c113e325b5c95a4e04b4254c
Abdulhmeed Harrass <392024733@stu.iu.edu.sa>
2023-01-22 01:27:28 +0300

    v0.9

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ hrkat = {"fatha":"\u0618", "dmah":"\u0619","ksrah":"\u061A","fathatan":"\u064B",
 #    for sentence in sentences:
 #        print(sentence)
 
-text = u'اكل الطالب التافحة'
+text = u'إذهب إلى الجامعة'
 
 #Tagger = Server._3RreEB("http://139.59.210.136:9005",1)
 #Parsed_Text = Tagger.Tag(text)
@@ -54,83 +54,109 @@ def extract_values(d):
 TreeType = nltk_tree_to_python(Parsed_Text)
 result = []
 for key, val in TreeType.items():
-    for k, v in val[0].items():
-        if k == "VP":
-            print(k)
-            print("جملة فعلية")
-            counter = 0
-            for word in v:
-                if isinstance(word, dict):
-                    for k1, v1 in word.items():
-                        print(k1)
-                        if k1 == "VBD":
-                            print("فعل ماضي")
-                            for v2 in v1:
-                                if isinstance(v2, str):
-                                    result.append(v2+hrkat["fatha"])
-                                elif isinstance(v2, dict):
-                                    for k3, v3 in v2.items():
-                                        result.append(v3[0])
-                            counter+=1
-                            break
-                        elif k1 == "VBP":
-                            print("فعل مضارع")
-                            for v2 in v1:
-                                if isinstance(v2, str):
-                                    result.append(v2+hrkat["dmah"])
-                                elif isinstance(v2, dict):
-                                    for k3, v3 in v2.items():
-                                        result.append(v3[0])
-                            counter+=1
-                            break
-                        elif ((k1 == "NP") and (counter == 1)):
-                            print("فاعل")
-                            for v2 in v1:
-                                if isinstance(v2, str):
-                                    if v2[:2] == "ال":
-                                        result.append(v2+hrkat["dmah"])
-
-                                    else:
-                                        result.append(v2+hrkat["dmatan"])
-
-                                elif isinstance(v2, dict):
-                                    for k3, v3 in v2.items():
-                                        if v3[0][:2] == "ال":
-                                            result.append(v3[0] + hrkat["dmah"])
-
-                                        else:
-                                            result.append(v3[0]+hrkat["dmatan"])
-                                counter += 1
+    if key =="S":
+        print("تم التعرف على الجملة يا بابا")
+        for k, v in val[0].items():
+            if k == "VP":
+                print(k)
+                print("جملة فعلية")
+                counter = 0
+                for word in v:
+                    if isinstance(word, dict):
+                        for k1, v1 in word.items():
+                            print(k1)
+                            if k1 == "VBD":
+                                print("فعل ماضي")
+                                for v2 in v1:
+                                    if isinstance(v2, str):
+                                        result.append(v2+hrkat["fatha"])
+                                    elif isinstance(v2, dict):
+                                        for k3, v3 in v2.items():
+                                            result.append(v3[0])
+                                counter+=1
                                 break
-                            break
-                        elif (((k1 == "NP") and (counter == 2)) or ((k1 == "DTJJ") and (counter == 2))):
-                            print("مفعول به")
+                            elif k1 == "VBP":
+                                print("فعل مضارع")
+                                for v2 in v1:
+                                    if isinstance(v2, str):
+                                        result.append(v2+hrkat["dmah"])
+                                    elif isinstance(v2, dict):
+                                        for k3, v3 in v2.items():
+                                            result.append(v3[0])
+                                counter+=1
+                                break
+                            elif k1 == "NP":
+                                print("فاعل")
+                                for v2 in v1:
+                                    if counter == 1:
+                                        if isinstance(v2, str):
+                                            if v2[:2] == "ال":
+                                                result.append(v2+hrkat["dmah"])
+
+                                            else:
+                                                result.append(v2+hrkat["dmatan"])
+
+                                        elif isinstance(v2, dict):
+                                            for k3, v3 in v2.items():
+                                                if v3[0][:2] == "ال":
+                                                    result.append(v3[0] + hrkat["dmah"])
+
+                                                else:
+                                                    result.append(v3[0]+hrkat["dmatan"])
+                                    elif counter == 2:
+                                        if isinstance(v2, str):
+                                            result.append(v2 + hrkat["fatha"])
+                                        elif isinstance(v2, dict):
+                                            for k3, v3 in v2.items():
+                                                result.append(v3[0] + hrkat["fatha"])
+                                    counter += 1
+                                break
+                    """
+                            elif (((k1 == "NP") and (counter == 2)) or ((k1 == "DTJJ") and (counter == 2))):
+                                print("مفعول به")
+                                for v2 in v1:
+                                    if isinstance(v2, str):
+                                        result.append(v2+hrkat["fatha"])
+                                    elif isinstance(v2, dict):
+                                        for k3, v3 in v2.items():
+                                            result.append(v3[0]+hrkat["fatha"])
+                                break
+                            #---------------------------------
                             for v2 in v1:
                                 if isinstance(v2, str):
-                                    result.append(v2+hrkat["fatha"])
+                                    result.append(v2)
                                 elif isinstance(v2, dict):
-                                    for k3, v3 in v2.items():
-                                        result.append(v3[0]+hrkat["fatha"])
-                            break
-                        #---------------------------------
-                        for v2 in v1:
-                            if isinstance(v2, str):
-                                result.append(v2)
-                            elif isinstance(v2, dict):
-                                for k3,v3 in v2.items():
-                                    result.append(v3[0])
-                """counter += 1
-                break
-                
-                elif isinstance(word, list):
-                    for item in word:
-                        result.append(item[0])
-                """
+                                    for k3,v3 in v2.items():
+                                        result.append(v3[0])
+                    counter += 1
+                    break
+                    
+                    elif isinstance(word, list):
+                        for item in word:
+                            result.append(item[0])
+                    """
 
-        if k == "NP":
-            print("جملة أسميه")
-        print(counter)
-ceko = 0
+            if k == "NP":
+                print("جملة أسميه")
+                print(type(v))
+                if isinstance(v, str):
+                    if v[:2] == "ال":
+                        result.append(v + hrkat["dmah"])
+
+                    else:
+                        result.append(v + hrkat["dmatan"])
+
+                elif isinstance(v, list):
+                    for v3 in v:
+                        if v3[:2] == "ال":
+                            result.append(v3 + hrkat["dmah"])
+
+                        else:
+                            result.append(v3+ hrkat["dmatan"])
+    else:
+        print("لم يتم التعرف على الجملة اتسوق امها يا غلام")
+
+
 print(" ".join(result))
 
 print(TreeType)
